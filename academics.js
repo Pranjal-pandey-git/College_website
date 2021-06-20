5fc4376f8ba5f7467f95021c9f52e2e9f515eea5
Pranjal-pandey-git <Acer@LAPTOP-LJ33H0H6.(none)>
2021-06-20 11:37:29 +0530

    First Commit

---
+++ academics.js
@@ -0,0 +1,6 @@
+function myFunction(element) {
+    
+    document.getElementById("clk").innerHTML = element.innerText;
+    document.getElementById("clk2").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. In itaque nam placeat dolor, impedit deleniti. 
+    Obcaecati dolor neque earum, tempore explicabo dicta facilis nihil, expedita quis ea laborum blanditiis. Ipsa.Lorem ipsum dolor sit amet consectetur adipisicing elit. In itaque nam placeat dolor, impedit deleniti. Obcaecati dolor neque earum, tempore explicabo dicta facilis nihil, expedita quis ea laborum blanditiis. Ipsa.";
+  }
